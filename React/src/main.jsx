1de12a4fea79bcbce880ae1dab5b7efa5be10305
Khalid54524 <abuubakar1@gmail.com>
2025-05-29 22:17:56 +0200

    klar

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 import { Provider } from './Provider.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Provider>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  )
