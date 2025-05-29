9a3b2a39e099062416d3d0cdcb4f356dcebe2bd2
Khalid54524 <abuubakar1@gmail.com>
2025-05-29 14:04:25 +0200

    first commit

---
+++ React/src/App.jsx
@@ -0,0 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./Home";
+import Profile from "./Profile";
+import Login from "./Login";
+import SignUp from "./SignUp";
