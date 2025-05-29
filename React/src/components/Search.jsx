6713b7e2100be54b3f5050ea2e7151eaf29b3337
Khalid54524 <abuubakar1@gmail.com>
2025-05-29 11:36:40 +0200

    first commit

---
+++ React/src/components/Search.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function Search({ searchQuery, handleSearch, searchResult }) {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchQuery}
+        onChange={handleSearch}
+        className="search-input"
+      />
+      {searchResult && searchResult.users && searchResult.users.length > 0 && (
+        <ul>
+          {searchResult.users.map((user) => (
+            <li key={user.id} data-testid={user.username}>
+              <Link to={`/profile/${user.id}`}>
+                {user.name} @ {user.username}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
