cc4d9812686d808041ac6d90e6f8064a483aff22
Khalid54524 <abuubakar1@gmail.com>
2025-05-29 22:14:41 +0200

    andra commit

--- React/src/Service.jsx
+++ React/src/Service.jsx
@@ -28,3 +28,102 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+const signup = async (newUser) => {
+    try {
+      if (newUser.password === newUser.verifyPassword) {
+        delete newUser.verifyPassword;
+        const resp = await api.post(`/signup`, newUser);
+        if (resp.status !== 201) {
+          throw new Error("Malfunctioning server GET request");
+        }
+        return resp.data;
+      } else {
+        throw new Error("Password Mismatch");
+      }
+    } catch (err) {
+      console.error(err.message);
+      return null;
+    }
+  };
+  
+  const login = async (loginData) => {
+    try {
+      const resp = await api.post(`/login`, loginData);
+      if (resp.status === 200) { // Store authentication token in session storage upon successful login
+        sessionStorage.setItem("userId", resp.data.userId);
+        return resp.data;
+      } else {
+        throw new Error("Login failed");
+      }
+    } catch (err) {
+      console.error(err.message);
+      return null;
+    }
+  };
+  
+  const logout = () => {
+    sessionStorage.removeItem("userId");
+  };
+  
+  const getSessionUserProfile = () => {
+    return api.get(`/profile`);
+  };
+  
+  const getUserProfile = (id) => {
+    return api.get(`/users/${id}`);
+  };
+  
+  const postTweet = (tweetText) => {
+    return api.post(`/tweets`, {
+      text: tweetText,
+    });
+  };
+  
+  const getTweets = async () => {
+    try {
+      const resp = await api.get("/tweets");
+  
+      if (resp.status !== 200) {
+        throw new Error("Malfunctioning server GET request");
+      }
+      return resp.data;
+    } catch (err) {
+      console.error(err.message);
+      return [];
+    }
+  };
+  
+  const getUserTweets = (id) => {
+    return api.get(`/tweets/users/${id}`);
+  };
+  
+  const getTrendingHashtags = () => {
+    return api.get(`/tweets/trending`);
+  };
+  
+  const followUser = (id) => {
+    return api.post(`/users/${id}/follow`, {});
+  };
+  
+  const unfollowUser = (id) => {
+    return api.post(`/users/${id}/unfollow`, {});
+  };
+  
+  const search = (find) => {
+    return api.get(`/search?find=${find}`);
+  };
+  
+  export {
+    signup,
+    login,
+    logout,
+    getSessionUserProfile,
+    getUserProfile,
+    postTweet,
+    getTweets,
+    getUserTweets,
+    getTrendingHashtags,
+    followUser,
+    unfollowUser,
+    search,
+  };
