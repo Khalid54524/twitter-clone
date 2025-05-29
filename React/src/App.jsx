631960e8244b239a1fc9acc1c54b869110e70398
Khalid54524 <abuubakar1@gmail.com>
2025-05-29 14:05:01 +0200

    second commit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,3 +3,17 @@ import Home from "./Home";
 import Profile from "./Profile";
 import Login from "./Login";
 import SignUp from "./SignUp";
+
+export default function App() {
+
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home/:id" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </Router>
+    )
+  }
