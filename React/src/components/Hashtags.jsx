52545e7dff08493999076f3dbd85ca3e4de5f4fc
Khalid54524 <abuubakar1@gmail.com>
2025-05-29 11:35:30 +0200

    done

--- React/src/components/Hashtags.jsx
+++ React/src/components/Hashtags.jsx
@@ -2,4 +2,11 @@ export default function Hashtags ({ hashtags }) {
     return (
       <div className="trending-hashtags">
         <h3>Trending Hashtags</h3>
-        
+        <ul>
+        {hashtags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}; 
